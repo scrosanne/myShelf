df53b88614fbea3e409523c8a6ccd15c2f450047
Rosanne Schönig <rosanne.schoenig@gmx.de>
2022-12-20 12:34:13 +0100

    textarea post changed

--- client/src/components/content/PostForm.jsx
+++ client/src/components/content/PostForm.jsx
@@ -39,8 +39,8 @@ export default function PostForm({ id, getAllPosts }) {
             <div className="post-top">
                 <p className="error-post-form">{error}</p>
                 <textarea
-                    rows="6"
-                    cols="7"
+                    // rows="6"
+                    // cols="7"
                     type="text"
                     name="content"
                     placeholder="choose a category and write somethig here!"
